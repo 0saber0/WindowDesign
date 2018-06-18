ddf2c07c12b435762603f5c473d2cd0402b19977
0saber0 <519888682@qq.com>
2018-06-18 12:55:39 +0800

    WindowDesign
    Delete unnecessary configuration files and compiled files

--- window-web/target/window-web-0.0.1-SNAPSHOT/js/service/overviewService.js
+++
@@ -1,8 +0,0 @@
-app.service('overviewService', function($http) {
-
-	this.search = function(pageNum, pageSize, searchOption) {
-		return $http.post('../overview/search.do?pageNum=' + pageNum
-				+ '&pageSize=' + pageSize, searchOption);
-	};
-	
-});
